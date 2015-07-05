9573952bc86baf55526df24715fa2b580ca0ec2e
Nate Collings <nate@natecollings.com>
2015-07-05 14:36:42 -0600

    Change the basic tutorial info

--- scripting/levels/l1.rb
+++ scripting/levels/l1.rb
@@ -2,26 +2,27 @@ module Scripts
   module Level1
     def self.play
       Script.new do
-        level_name 'Level 1'
-        greeting
+        level_name 'Tutorial'
 
-        expect_cmd :help, 'Type in "help" to get a list of commands' do
-          output 'Success! You are 1337 haxor', :info
+        output "HAKSH welcomes you. You will be guided through the basics of this system.", :info
+
+        expect_cmd :help, "'help' will show you a list of commands. As you prove your skills more commands will be made available." do
+          output 'Success. You can use the help command at any time to view your currently available commands.', :info
         end
 
-        expect_cmd :pwd, 'Use the command "pwd" to get your current location!' do
-          output "Success! Now you'll always now where you are.", :info
+        expect_cmd :pwd, "'pwd' will tell you where you are in the filesystem." do
+          output 'Success. Use pwd whenever you are unsure of your location', :info
         end
 
-        expect_cmd :ls, '"ls" can be used to get a list of files and directories' do
-          output 'Nice job! We call them "Directories" not folders, FYI', :info
+        expect_cmd :ls, "'ls' will list the contents of the current directory" do
+          output 'Success. Contents can be files or directories.', :info
         end
 
-        expect_cmd_with_args :man, [:man], 'manual pages can be used with the "man" command. Read the manual page for the man command' do
-          output 'Nice job, man', :info
+        expect_cmd_with_args :man, [:man], "You can learn more about commands with the 'man' command. man is short for manual. Read the manual for the 'man' command." do
+          output 'Some commands may be missing a manual page.', :info
         end
 
-        run_playground 'This level is over, but you can keeping playing with the commands you have learned'
+        run_playground 'This level is now over. You may now use the commands you have learned thus far freely.'
       end
     end
   end
