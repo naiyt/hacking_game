e54c8d94cced005f3e7008d3cf528c61eb1c8d36
Nate Collings <nate@natecollings.com>
2015-07-05 21:04:42 -0600

    cleaning up scripting class

--- scripting/scripting.rb
+++ scripting/scripting.rb
@@ -12,26 +12,18 @@ module Scripts
 
     def initialize(&block)
       @shell = Scripts.shell
-
       instance_eval(&block)
     end
+
+    # DSL methods
 
     def level_name(n)
       @name = n
     end
 
-    def next_cmds
-      @shell.run(forever=false) do |res, cmds|
-        @latest_cmds = cmds
-        @latest_res = res
-      end
-    end
-
     def output(text, type=:standard)
-      unless text.nil?
-        # sends to the Commands::OutputHelper methods
-        puts self.send(type, text)
-      end
+      # sends to the Commands::OutputHelper methods
+      puts self.send(type, text) unless text.nil?
     end
 
     def expect_cmd(cmd, txt=nil)
@@ -46,6 +38,28 @@ module Scripts
       yield
     end
 
+    def run_playground(txt=nil)
+      output(txt, :info)
+      @shell.run(forever=true)
+    end
+
+    def greeting
+      output("Welcome to #{@name}", :info)
+    end
+
+    def available_commands(cmds)
+      Commands.available_commands = cmds
+    end
+
+    private
+
+    def next_cmds
+      @shell.run(forever=false) do |res, cmds|
+        @latest_cmds = cmds
+        @latest_res = res
+      end
+    end
+
     def latest_cmd?(cmd)
       unless @latest_cmds.nil?
         @latest_cmds.map { |c| c[:cmd] }.include? cmd.to_sym
@@ -56,19 +70,6 @@ module Scripts
       unless @latest_cmds.nil?
         args == @latest_cmds.map { |c| c[:args].map { |a| a.to_sym} }.flatten
       end
-    end
-
-    def run_playground(txt=nil)
-      output(txt, :info)
-      @shell.run(forever=true)
-    end
-
-    def greeting
-      output("Welcome to #{@name}!", :info)
-    end
-
-    def available_commands(cmds)
-      Commands.available_commands = cmds
     end
   end
 end
