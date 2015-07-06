be6626041dc0b990466ccd25f92e58254229c7f3
Nate Collings <nate@natecollings.com>
2015-07-05 21:26:27 -0600

    move FS exceptions to seperate file

--- filesystem/filesystem.rb
+++ filesystem/filesystem.rb
@@ -1,25 +1,8 @@
 require 'singleton'
 require 'yaml'
+require_relative 'exceptions'
 
 module Filesystem
-  class AlreadyExistsError < StandardError
-  end
-
-  class FileDoesNotExistError < StandardError
-  end
-
-  class DirectoryNotEmptyError < StandardError
-  end
-
-  class FileAlreadyExists < StandardError
-  end
-
-  class PathDoesNotExist < StandardError
-  end
-
-  class FileNotDir < StandardError
-  end
-
   class Table
     include Singleton
     attr_accessor :table
