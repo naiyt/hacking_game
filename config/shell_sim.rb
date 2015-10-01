ff99e984d7496ecc66f4c3a4a3a54a310b139acb
Nate Collings <nate@natecollings.com>
2015-09-30 21:30:00 -0600

    rename fs and passwd to indicate that they are defaults

--- config/shell_sim.rb
+++ config/shell_sim.rb
@@ -2,7 +2,7 @@ require 'yaml'
 require 'shell_sim'
 
 ShellSim.configure do |config|
-  config.fs_data    = YAML.load_file('config/fs.yml')
-  config.users_data = YAML.load_file('config/passwd.yml')
+  config.fs_data    = YAML.load_file('config/default_fs.yml')
+  config.users_data = YAML.load_file('config/default_users.yml')
 end
 
